d650bb3f98b2f5a7674ae572ef5cc1836c679ced
abrinkmacmu <brinkap@gmail.com>
2017-02-21 15:51:47 -0500

    RRTConnect fully functional

--- RRTConnectPlanner.py
+++ RRTConnectPlanner.py
@@ -30,7 +30,7 @@ class RRTConnectPlanner(object):
         print "goalConfig = [%.2f, %.2f]" %(goal_config[0], goal_config[1])
         disc = True;
         while (disc):
-            if(random.random() < 0.9):
+            if(random.random() < 0.95):
                 desConfig = self.planning_env.GenerateRandomConfiguration();
             else:
                 desConfig = goal_config;
@@ -45,7 +45,7 @@ class RRTConnectPlanner(object):
             #if(numpy.array_equal(extension, goal_config)):
             #    disc = False
 
-            if(random.random() < 0.9):
+            if(random.random() < 0.95):
                 desConfig = self.planning_env.GenerateRandomConfiguration();
             else:
                 desConfig = start_config;
@@ -68,84 +68,33 @@ class RRTConnectPlanner(object):
                         rtree.AddEdge(extIDr, lastIDr);
                         self.planning_env.PlotEdge(extension, lastlink);
                         disc = False
+                        extIDf = i
                         break
-
-        fplan.insert(0,start_config)
-        fplan.append(goal_config)
-        while 1:
-            extIDf = ftree.edges[extIDf];
-            currConfig = ftree.vertices[extIDf];
-            if (extIDf == ftree.GetRootId()):
-                break;
-            else:
-                fplan.insert(1, currConfig);
-
-        for config in fplan:
-            print "fconfig = [%.2f, %.2f]" %(config[0], config[1])
-        print("--- %s seconds ---" % (time.time() - start_time))
-        
+        lastlink_cp = lastlink
         rplan.insert(0,goal_config)
-        rplan.append(start_config)
         while 1:
-            extIDr = rtree.edges[extIDr];
-            currConfig = rtree.vertices[extIDr];
             if (extIDr == rtree.GetRootId()):
                 break;
             else:
-                rplan.insert(1, currConfig);
+                rplan.insert(1, extension);
+            extIDr = rtree.edges[extIDr];
+            extension = rtree.vertices[extIDr];
 
-        for config in rplan:
-            print "fconfig = [%.2f, %.2f]" %(config[0], config[1])
+ 
+        plan = list(reversed(rplan))
 
-        import IPython
-        IPython.embed()
-
-        return rplan
-    '''
-            if(random.random() < .9):
-                desConfig = self.planning_env.GenerateRandomConfiguration();
-            else:
-                desConfig = start_config;
-            #newCurrConfig = self.planning_env.GenerateRandomConfiguration();
-
-            [nearID, nearConfig] = rtree.GetNearestVertex(desConfig);
-            print "desConfig = [%.2f, %.2f]" %(desConfig[0], desConfig[1])
-            print "nearID = %d, nearConfig = [%.2f, %.2f]" %(nearID, nearConfig[0], nearConfig[1])
-            
-            extension = self.planning_env.Extend(nearConfig, desConfig)
-            print extension
-
-            if (extension != None):
-                desConfig = extension
-                desIDr = rtree.AddVertex(desConfig);
-                rtree.AddEdge(nearID, desIDr);
-
-                #plan.append(currConfig)
-
-                print "desIDr = %d, desConfig = [%.2f, %.2f]" %(desIDr, desConfig[0], desConfig[1])
-                self.planning_env.PlotEdge(nearConfig, desConfig);
-                #time.sleep(1)
-                for i in range(len(ftree.vertices)):
-                    if (numpy.linalg.norm(numpy.array(desConfig) - numpy.array(ftree.vertices[i])) < 2):
-                        currIDf = i
-                        #[desIDf, desConfigf] = ftree.GetNearestVertex(ftree.vertices[i])
-                        #extension = self.planning_env.Extend(desConfig, desConfigf)
-                        #if (numpy.array_equal(extension,desConfigf)):
-                            #desIDr = rtree.AddVertex(desConfigf);
-                            #rtree.AddEdge(desIDr, desIDf);
-                            #self.planning_env.PlotEdge(desConfig, desConfigf);
-                        disc = False
-                        break
-        #currConfig = goal_config
-        #currIDf = goalID;
-        rplan.append(goal_config)
+        plan.insert(0,start_config)
         while 1:
-            desIDr = rtree.edges[desIDr];
-            desConfig = rtree.vertices[desIDr];
-            if (desIDr == rtree.GetRootId()):
+            if (extIDf == ftree.GetRootId()):
                 break;
             else:
-                rplan.insert(1,desConfig);
-      
+                plan.insert(1, lastlink);
+            extIDf = ftree.edges[extIDf];
+            lastlink = ftree.vertices[extIDf];
 
-        #plan = list(reversed(plan))'''
+
+        for config in plan:
+            print "fconfig = [%.2f, %.2f]" %(config[0], config[1])
+        print("--- %s seconds ---" % (time.time() - start_time))
+
+        return plan
