9f1d14e60cad2c5f499585c72a7dacea10dd8433
abrinkmacmu <brinkap@gmail.com>
2017-02-21 14:36:46 -0500

    partial RRTConnect, big revamp

--- RRTConnectPlanner.py
+++ RRTConnectPlanner.py
@@ -15,6 +15,8 @@ class RRTConnectPlanner(object):
         ftree = RRTTree(self.planning_env, start_config)
         rtree = RRTTree(self.planning_env, goal_config)
         plan = []
+        rplan = []
+        fplan = []
 
         if self.visualize and hasattr(self.planning_env, 'InitializePlot'):
             self.planning_env.InitializePlot(goal_config)
@@ -24,110 +26,126 @@ class RRTConnectPlanner(object):
         #  and n is the dimension of the robots configuration space
 
 
-
-        currConfig = start_config;
-        currIDf = ftree.GetRootId();
-        currIDr = rtree.GetRootId();
-
         print "startConfig = [%.2f, %.2f]" %(start_config[0], start_config[1])
         print "goalConfig = [%.2f, %.2f]" %(goal_config[0], goal_config[1])
-        #while (self.planning_env.Extend(currConfig, goal_config) == None):
         disc = True;
-        while (disc):#self.planning_env.ComputeDistance(currConfig,goal_config) > epsilon):
-            if(random.random() < .9):
-                newCurrConfig = self.planning_env.GenerateRandomConfiguration();
+        while (disc):
+            if(random.random() < 0.9):
+                desConfig = self.planning_env.GenerateRandomConfiguration();
             else:
-                newCurrConfig = goal_config;
-            #newCurrConfig = self.planning_env.GenerateRandomConfiguration();
+                desConfig = goal_config;
 
-            [nearID, nearConfig] = ftree.GetNearestVertex(newCurrConfig);
-            print "newCurrConfig = [%.2f, %.2f]" %(newCurrConfig[0], newCurrConfig[1])
-            print "nearID = %d, nearConfig = [%.2f, %.2f]" %(nearID, nearConfig[0], nearConfig[1])
-            
-            extension = self.planning_env.Extend(nearConfig, newCurrConfig)
-            print extension
-
+            [nearID, nearConfig] = ftree.GetNearestVertex(desConfig);            
+            extension = self.planning_env.Extend(nearConfig, desConfig)
             if (extension != None):
-                currConfig = extension
-                currIDf = ftree.AddVertex(currConfig);
-                ftree.AddEdge(nearID, currIDf);
+                extIDf = ftree.AddVertex(extension);
+                ftree.AddEdge(nearID, extIDf);
+                self.planning_env.PlotEdge(nearConfig, extension);
+            
+            #if(numpy.array_equal(extension, goal_config)):
+            #    disc = False
 
-                #plan.append(currConfig)
-
-                print "currIDf = %d, currConfig = [%.2f, %.2f]" %(currIDf, currConfig[0], currConfig[1])
-                self.planning_env.PlotEdge(nearConfig, currConfig);
-
-
-
-            if(random.random() < .9):
-                newCurrConfig = self.planning_env.GenerateRandomConfiguration();
+            if(random.random() < 0.9):
+                desConfig = self.planning_env.GenerateRandomConfiguration();
             else:
-                newCurrConfig = start_config;
-            #newCurrConfig = self.planning_env.GenerateRandomConfiguration();
+                desConfig = start_config;
 
-            [nearID, nearConfig] = rtree.GetNearestVertex(newCurrConfig);
-            print "newCurrConfig = [%.2f, %.2f]" %(newCurrConfig[0], newCurrConfig[1])
-            print "nearID = %d, nearConfig = [%.2f, %.2f]" %(nearID, nearConfig[0], nearConfig[1])
-            
-            extension = self.planning_env.Extend(nearConfig, newCurrConfig)
-            print extension
-
+            [nearID, nearConfig] = rtree.GetNearestVertex(desConfig);            
+            extension = self.planning_env.Extend(nearConfig, desConfig)
             if (extension != None):
-                currConfig = extension
-                currIDr = rtree.AddVertex(currConfig);
-                rtree.AddEdge(nearID, currIDr);
+                extIDr = rtree.AddVertex(extension);
+                rtree.AddEdge(nearID, extIDr);
+                self.planning_env.PlotEdge(nearConfig, extension);
+            
+            #if(numpy.array_equal(extension, start_config)):
+            #    disc = False
 
-                #plan.append(currConfig)
-
-                print "currIDr = %d, currConfig = [%.2f, %.2f]" %(currIDr, currConfig[0], currConfig[1])
-                self.planning_env.PlotEdge(nearConfig, currConfig);
-            #time.sleep(1)
             for i in range(len(ftree.vertices)):
-                if (numpy.linalg.norm(numpy.array(currConfig) - numpy.array(ftree.vertices[i])) < 2):
-                    [currIDf, newCurrConfig] = ftree.GetNearestVertex(ftree.vertices[i])
-                    extension = self.planning_env.Extend(currConfig, newCurrConfig)
-                    if (numpy.array_equal(extension,newCurrConfig)):
-                        currIDr = rtree.AddVertex(currConfig);
-                        rtree.AddEdge(currIDf, currIDr);
-                        self.planning_env.PlotEdge(newCurrConfig, currConfig);
+                if (numpy.linalg.norm(numpy.array(desConfig) - numpy.array(ftree.vertices[i])) < 2):
+                    lastlink = self.planning_env.Extend(extension, ftree.vertices[i])
+                    if (numpy.array_equal(lastlink,ftree.vertices[i])):
+                        lastIDr = rtree.AddVertex(lastlink);
+                        rtree.AddEdge(extIDr, lastIDr);
+                        self.planning_env.PlotEdge(extension, lastlink);
                         disc = False
                         break
 
-        #goalID = ftree.AddVertex(goal_config);
-        #ftree.AddEdge(currIDf, goalID);
-        #self.planning_env.PlotEdge(currConfig, goal_config)
-
-        #currConfig = goal_config
-        #currIDf = goalID;
-               
-        plan.append(goal_config)
-        plan.append(start_config)
+        fplan.insert(0,start_config)
+        fplan.append(goal_config)
         while 1:
-            currIDr = rtree.edges[currIDr];
-            currConfig = rtree.vertices[currIDr];
-            if (currIDr == rtree.GetRootId()):
+            extIDf = ftree.edges[extIDf];
+            currConfig = ftree.vertices[extIDf];
+            if (extIDf == ftree.GetRootId()):
                 break;
             else:
-                plan.insert(1, currConfig);
-        
+                fplan.insert(1, currConfig);
 
-        plan = list(reversed(plan))
-
-        while 1:
-            currIDf = ftree.edges[currIDf];
-            currConfig = ftree.vertices[currIDf];
-            if (currIDf == ftree.GetRootId()):
-                break;
-            else:
-                plan.insert(1, currConfig);
-
-        
-
-
-
+        for config in fplan:
+            print "fconfig = [%.2f, %.2f]" %(config[0], config[1])
         print("--- %s seconds ---" % (time.time() - start_time))
+        
+        rplan.insert(0,goal_config)
+        rplan.append(start_config)
+        while 1:
+            extIDr = rtree.edges[extIDr];
+            currConfig = rtree.vertices[extIDr];
+            if (extIDr == rtree.GetRootId()):
+                break;
+            else:
+                rplan.insert(1, currConfig);
+
+        for config in rplan:
+            print "fconfig = [%.2f, %.2f]" %(config[0], config[1])
+
         import IPython
         IPython.embed()
-        for config in plan:
-            print "config = [%.2f, %.2f]" %(config[0], config[1])
-        return plan
+
+        return rplan
+    '''
+            if(random.random() < .9):
+                desConfig = self.planning_env.GenerateRandomConfiguration();
+            else:
+                desConfig = start_config;
+            #newCurrConfig = self.planning_env.GenerateRandomConfiguration();
+
+            [nearID, nearConfig] = rtree.GetNearestVertex(desConfig);
+            print "desConfig = [%.2f, %.2f]" %(desConfig[0], desConfig[1])
+            print "nearID = %d, nearConfig = [%.2f, %.2f]" %(nearID, nearConfig[0], nearConfig[1])
+            
+            extension = self.planning_env.Extend(nearConfig, desConfig)
+            print extension
+
+            if (extension != None):
+                desConfig = extension
+                desIDr = rtree.AddVertex(desConfig);
+                rtree.AddEdge(nearID, desIDr);
+
+                #plan.append(currConfig)
+
+                print "desIDr = %d, desConfig = [%.2f, %.2f]" %(desIDr, desConfig[0], desConfig[1])
+                self.planning_env.PlotEdge(nearConfig, desConfig);
+                #time.sleep(1)
+                for i in range(len(ftree.vertices)):
+                    if (numpy.linalg.norm(numpy.array(desConfig) - numpy.array(ftree.vertices[i])) < 2):
+                        currIDf = i
+                        #[desIDf, desConfigf] = ftree.GetNearestVertex(ftree.vertices[i])
+                        #extension = self.planning_env.Extend(desConfig, desConfigf)
+                        #if (numpy.array_equal(extension,desConfigf)):
+                            #desIDr = rtree.AddVertex(desConfigf);
+                            #rtree.AddEdge(desIDr, desIDf);
+                            #self.planning_env.PlotEdge(desConfig, desConfigf);
+                        disc = False
+                        break
+        #currConfig = goal_config
+        #currIDf = goalID;
+        rplan.append(goal_config)
+        while 1:
+            desIDr = rtree.edges[desIDr];
+            desConfig = rtree.vertices[desIDr];
+            if (desIDr == rtree.GetRootId()):
+                break;
+            else:
+                rplan.insert(1,desConfig);
+      
+
+        #plan = list(reversed(plan))'''
